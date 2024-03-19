572866c36efd53fa6451dfad4214e0e9f56ca897
KUN119 <kjp2629@naver.com>
2024-03-19 20:21:49 +0900

    useEffect 사용하기

---
+++ src/AppProducts.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Products from './components/Products'
+
+export default function AppProducts() {
+    const [showProdutcs, setShowProdutcs] = useState(true)
+
+    return (
+        <div>
+            {showProdutcs && <Products />}
+            <button onClick={() => setShowProdutcs((show) => !show)}>Toggle</button>
+        </div>
+    )
+}
