c5cd0db717ffc3d3a29779b1db939497cc57f215
KUN119 <kjp2629@naver.com>
2024-03-21 16:54:27 +0900

    useState, 전개연산자 사용하기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './AppXY.css';
 
 export default function AppXY() {
-  return (
-    <div>AppXY</div>
-  )
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+
+        // 만약 수평(x축)으로만 이동하고 싶다면? -----------------------
+        // setPosition(prev => ({ x: e.clientX, y: prev.y }));
+        // setPosition(prev => ({ ...prev, x: e.clientX }));
+        // ----------------------------------------------------
+    }
+    
+    return (
+        <div className='container' onMouseMove={handleMouseMove}>
+            <div
+                className='pointer'
+                style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+            />
+        </div>
+    )
 }
+ 
